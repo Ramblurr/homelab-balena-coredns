2d6007ae4d07d8f449f872defebda082c52230ab
Casey Link <unnamedrambler@gmail.com>
2021-01-05 20:59:19 +0100

    remove handover strategy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,40 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-wait_for_handover() {
-    echo [$(date "+%Y-%m-%d %H:%M:%S")] "Waiting for next handover"
-    while [ ! -f /data/hand-over-waiting ]; do
-        sleep 5
-    done
-    echo [$(date "+%Y-%m-%d %H:%M:%S")] "New sibling requested handover, acking"
-    touch /data/hand-over-ack
-    touch /data/resin-kill-me
-}
-
-if [ ! -f /data/hand-over-booted ]; then
-    # this is our first boot so there is no sibling
-    touch /data/hand-over-booted
-else
-    # signal sibling we are waiting for the handover
-    touch /data/hand-over-waiting
-    while [ ! -f /data/hand-over-ack ]; do
-        echo [$(date "+%Y-%m-%d %H:%M:%S")] "Waiting for sibling to signal handover is ready"
-        sleep 2
-    done
-    echo [$(date "+%Y-%m-%d %H:%M:%S")] "Sibling acked. Booting"
-
-    # cleanup and prepare for next handover
-    rm -f /data/hand-over-ack
-    rm -f /data/hand-over-waiting
-    rm -f /data/resin-kill-me
-
-    # give time for sibling container to be killed
-    sleep 2
-fi
-
-
-wait_for_handover &
-
 DNS_BIND_IFACE=${DNS_BIND_IFACE:-eth0}
 echo [$(date "+%Y-%m-%d %H:%M:%S")] "DNS service starting, fetching ip for ${DNS_BIND_IFACE}"
 export DNS_IP=$(ip -f inet addr show ${DNS_BIND_IFACE} | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
